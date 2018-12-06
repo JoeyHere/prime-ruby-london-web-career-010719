239894e470d311b97b30aef9c47f8a863d555b24
Joseph O'Dell <josephodell@outlook.com>
2018-12-06 15:41:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+
+def prime?(number)
+if number < 2
+  return false
+end
+array = (2...number).to_a
+array.each do |x|
+  if number % x == 0
+    return false
+  end
+end
+return true
+end
